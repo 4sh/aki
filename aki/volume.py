d5faf33377fcea3c01fc9282eef183058abc3ffc
nroulon <nicolas.roulon@4sh.fr>
2023-04-03 22:12:10 +0200

    fix: copy on host type with docker does not work

--- aki/volume.py
+++ aki/volume.py
@@ -191,7 +191,7 @@ class AkiHostVolume(AkiVolume):
         if platform == "linux" or platform == "linux2":
             print_verbose('copy on linux - start a container')
             self.docker_client.containers.run('busybox',
-                                              command='cp -R /source/ /destination',
+                                              command='cp -R /source/. /destination',
                                               volumes=[
                                                   f'{source.external_name}:/source',
                                                   f'{destination.external_name}:/destination'
