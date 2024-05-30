6fbe64af5d24b9d88593785f032bda6a5dfae898
nroulon <nicolas.roulon@4sh.fr>
2024-05-30 20:04:55 +0200

    chore: use poeblix for fix dependencies + replace tarball by wheels

--- release.sh
+++ release.sh
@@ -6,16 +6,16 @@ version="$1"
 
 echo "change version in files"
 echo "__version__ = '$version'" > "$script_directory/aki/version.py"
-sed -i'.bak' -e "s@^pipx install .*@pipx install https://github.com/4sh/aki/releases/download/v$version/aki-$version.tar.gz@" "README.md"
+sed -i'.bak' -e "s@^pipx install .*@pipx install https://github.com/4sh/aki/releases/download/v$version/aki-$version-py3-none-any.whl@" "README.md"
 sed -i'.bak' -e "s@^version = .*@version = \"$version\"@" "pyproject.toml"
 rm README.md.bak pyproject.toml.bak
 
 echo "poetry build"
-poetry build
+poetry blixbuild
 
 echo "commit and tag"
 git commit --all --message "release $version"
 git tag "v$version"
 
-echo "upload $script_directory/dist/aki-$version.tar.gz to a new release $version on https://github.com/4sh/aki/releases"
+echo "upload $script_directory/dist/aki-$version-py3-none-any.whl to a new release $version on https://github.com/4sh/aki/releases"
 echo "push for publish version and docker package : git push && git push --tags"
